70ed1f13406db5dbf634830a8062ea431760512e
Brian Duggan <bduggan3387@gmail.com>
2016-02-06 12:37:09 -0500

    sets up for heroku deployment

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var dbLink = proccess.env.MONGOLAB_URI || 'mongodb://localhost/otter_db01';
 
 app.use( express.static('./public') );
 app.use( bodyParser.urlencoded({extended: true}) );
 app.use( bodyParser.json() );
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://localhost/otter_db01');
+mongoose.connect(dbLink);
 
 app.get('/', function(req, res){
   res.render('index');
@@ -17,7 +18,7 @@ app.get('/', function(req, res){
 var postsRouter = require('./routes/posts');
 app.use('/posts', postsRouter);
 
-var port = 8080;
+var port = proccess.env.PORT || 8080;
 app.listen(port, function(){
   console.log('...the magic is happening on port ' + port);
 });
